0c1eb7ddce250177c84b3683c9e69f1b931f98ae
tvves <108936519+tvves@users.noreply.github.com>
2022-10-07 09:58:18 -0300

    Update estudo.js

--- estudo.js
+++ estudo.js
@@ -28,3 +28,16 @@ function mostraTabuada(){
         document.write("O valor do i " + i + "<br>");
     }
 }
+function total(){
+    let v = document.getElementById("valor").value;
+    let j = document.getElementById("juros").value;
+    let t = document.getElementById("meses").value;
+    let r = 0;
+    for(let i=1; i <=t; i++){
+        r = v * (1 +(j/100));
+        document.write("Mês " + " - valor: " + r + "<br>")
+        v = r;
+    }
+        
+    document.write("Resultado= "+ r);
+}
